ab703603261c16c1ab89b05b0fd732a91a3d1539
Joe Purvis <133743849+Joedaddy66@users.noreply.github.com>
2025-09-27 14:21:06 +0000

    Checkpoint from VS Code for coding agent session

---
+++ assets/pieceImages.ts
@@ -0,0 +1,17 @@
+// Placeholder files for custom chess piece images
+// Replace these with your actual PNGs
+
+export const pieceImagePaths = {
+  wK: require('./pieces/white_king_custom.png'), // White King
+  wQ: require('./pieces/white_queen_custom.png'), // White Queen
+  wR: require('./pieces/white_rook_custom.png'), // White Rook
+  wB: require('./pieces/white_bishop_custom.png'), // White Bishop
+  wN: require('./pieces/white_knight_custom.png'), // White Knight
+  wP: require('./pieces/white_pawn_custom.png'), // White Pawn
+  bK: require('./pieces/black_king_custom.png'), // Black King
+  bQ: require('./pieces/black_queen_custom.png'), // Black Queen
+  bR: require('./pieces/black_rook_custom.png'), // Black Rook
+  bB: require('./pieces/black_bishop_custom.png'), // Black Bishop
+  bN: require('./pieces/black_knight_custom.png'), // Black Knight
+  bP: require('./pieces/black_pawn_custom.png'), // Black Pawn
+};
